4ca2b5634eb3559273a7ef513a5f8f6877dc7498
Nikola Aničić <anicicn@yahoo.com>
2022-03-16 11:19:04 +0100

    Mobile version a bit updated
    On mobile screens should look a bit nicer, hamburger icon to be fixed

---
+++ script/index.js
@@ -0,0 +1,11 @@
+// function toggleMobileMenu(menu) {
+//     menu.classList.toggle('active');
+//     menu.classList.toggle('open');
+// }
+
+$(function() {
+  $(".toggle-menu").click(function() {
+    $(this).toggleClass("active");
+    $('.menu-drawer').toggleClass("open");
+  });
+});
